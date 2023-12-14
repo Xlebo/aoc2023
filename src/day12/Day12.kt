4c010adb6ded7b6759cf102bca94ed1c9b262621
Xlebo <samo.chlebo@gmail.com>
2023-12-14 16:07:04 +0100

    day12 fix

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -4,56 +4,57 @@ import getOrFetchInputData
 import readInput
 
 fun main() {
+    val cache = mutableMapOf<Pair<List<String>, List<Int>>, Long>()
 
-    val cache = mutableMapOf<Pair<String, List<Int>>, Int>()
-
-    fun fitDamagedToRow(row: String, damaged: List<Int>): Int {
-        return cache.getOrPut(row to damaged) {
+    fun fitDamagedToRow(row: String, damaged: List<Int>): Long {
+        return cache.getOrPut(listOf(row) to damaged) c@{
             if (damaged.isEmpty()) {
-                return if (row.contains('#')) 0 else 1
+                return@c if (row.contains('#')) 0L else 1L
             }
             val minRowLength = damaged.sum() + damaged.size - 1
             if (row.length < minRowLength) {
-                return 0
+                return@c 0L
             }
 
             if (row.length == damaged[0]) {
-                return 1
+                return@c 1L
             }
 
             val popFirstValue = if (!row.startsWith('#')) {
                 fitDamagedToRow(row.substring(1), damaged)
             } else {
-                0
+                0L
             }
 
             val rest = if (row[damaged[0]] == '?') {
                 fitDamagedToRow(row.substring(damaged[0] + 1), damaged.subList(1, damaged.size))
             } else {
-                0
+                0L
             }
 
-            return rest + popFirstValue
+            return@c rest + popFirstValue
         }
     }
 
-    fun fitDamagedIntoRow(rows: List<String>, damageds: List<Int>): Int {
-        if (rows.isEmpty() && damageds.isEmpty())
-            return 1
-        if (damageds.isNotEmpty() && rows.isEmpty()) {
-            return 0
-        }
-        if (rows.size == 1) {
-            return fitDamagedToRow(rows[0], damageds)
-        }
-        return (0..damageds.size).sumOf { damaged ->
-            val currentPoints = fitDamagedToRow(rows[0], damageds.subList(0, damaged))
-            val others = fitDamagedIntoRow(rows.subList(1, rows.size), damageds.subList(damaged, damageds.size))
-            currentPoints * others
+    fun fitDamagedIntoRow(rows: List<String>, damageds: List<Int>): Long {
+        return cache.getOrPut(rows to damageds) c@{
+            if (rows.isEmpty() && damageds.isEmpty())
+                return@c 1L
+            if (damageds.isNotEmpty() && rows.isEmpty()) {
+                return@c 0L
+            }
+            if (rows.size == 1) {
+                return@c fitDamagedToRow(rows[0], damageds)
+            }
+            return@c (0..damageds.size).sumOf { damaged ->
+                val currentPoints = fitDamagedToRow(rows[0], damageds.subList(0, damaged))
+                val others = fitDamagedIntoRow(rows.subList(1, rows.size), damageds.subList(damaged, damageds.size))
+                currentPoints * others
+            }
         }
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val parsedInput = input.map { it.split(" ") }
             .map {
                 it[0].split(".").filter { row -> row.isNotEmpty() } to it[1].split(",").map { number -> number.toInt() }
@@ -64,20 +65,31 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int {
-       // nebudu to dělat, ne bu du!
-        return 42
+    fun part2(input: List<String>): Long {
+        val parsedInput = input.map { it.split(" ") }
+            .map {
+                ((it[0] + "?").repeat(5).removeSuffix("?")).split(".").filter { row -> row.isNotEmpty() } to
+                        (it[1] + ",").repeat(5).split(",").filter { number -> number.isNotEmpty() }
+                            .map { number -> number.toInt() }
+            }
+
+        return parsedInput.sumOf {
+            var result = fitDamagedIntoRow(it.first, it.second)
+            println("${it.first} + ${it.second}: $result")
+            result
+        }
     }
 
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test", "day12")
     val result1 = part1(testInput)
-//    val result2 = part2(testInput)
-    check(result1 == 21) { "Got: $result1" }
-//    check(result2 == 525152) { "Got: $result2" }
+    val result2 = part2(testInput)
+    check(result1 == 21L) { "Got: $result1" }
+    check(result2 == 525152L) { "Got: $result2" }
+    println("check 2 successful")
 
     val input = getOrFetchInputData(12)
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
